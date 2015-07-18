b0f326b4b39c8a62945323445d5adb349be641ec
jojoldu <jojoldu@naver.com>
2015-07-18 11:33:22 +0900

    2주차 Email, Password  유효성 체크

--- js/selector.js
+++ js/selector.js
@@ -1,8 +1,11 @@
 $(function(){
 
-    //$('#btnLogin').click(show);
-    alert('위험');
-    $('#btnLogin').click(showEmail);
+    $('#btnLogin').click(checkIdAndPw);
+ 	window.emailList = ['tom', 'joe', 'hong', 'oh'];
+   
+
+    //1. 허용된 id만 로그인 가능
+    //2. password와 password2가 같아야함
 });
 
 function show(){
@@ -14,3 +17,24 @@ function showEmail(){
     var password = $('#inputPassword').val('');
     alert(email);
 }
+
+function checkIdAndPw(){
+	var $inputs = $('.inputs');
+	var required = true;
+
+	$.each($inputs, function(index, value){
+		var $input = $($inputs[index]);
+
+		//'', null, undefinded, 0, false
+		if(!$input.val()){// not false === true
+			required = false;
+		}
+	});
+
+	if(!required){
+		alert('값을 채워주세요');
+	}else{
+		alert('로그인!');
+	}
+}
+
