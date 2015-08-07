bb8aea736c81eeb0f11273cce1e4960eb091464d
jojoldu <jojoldu@gmail.com>
2015-08-08 08:34:41 +0900

    회원 수정기능 추가 및 회원등록 기능 작성중

--- js/member.js
+++ js/member.js
@@ -8,26 +8,47 @@ $(function(){
 var member = {
 	$el : {},
 	list : [],
+	currentData : {},
 
 	showModal : function(){
+		if(!currentData.no){
+			this.reset();
+			this.currentData.joinDate = this.dateFormat();
+		}
+
 		$('#memberModal').modal();
 	},
 
+	closeModal : function(){
+		$('#memberModal').modal('hide');
+	},
+	
+	reset : function(){
+		this.currentData = {};
+		$('#inputEmail').val('');
+		$('#inputName').val('');
+		$('#inputJob').val('');
+	},
+
 	init : function(){
-		this.list = this.generateMembers();
+		this.list = (this.list.length > 0)? this.list : this.generateMembers();
 		this.makeTbody(this.list);
 		this.$el = $('#memberMain');
 	
 		this.$el.on('click', '.member_info', function(){
-			member.edit(member.find(this));
+			var idx = $(this).attr('id').slice(7);
+			member.edit(member.find(idx));
 			member.showModal();
 		});
+
+		this.$el.find('#btnSubmit').click(this.save());
+		this.$el.find('#btnClose').click(this.closeModal());
 	},
 
 	generateMembers : function(){
 		var members = [
 			{
-				no			: 1,
+				idx			: 1,
 				email 	 	: 'jojoldu@gmail.com',
 				name 		: '이동욱',
 				job 		: 'web developer',
@@ -35,7 +56,7 @@ var member = {
 				updateDate 	: '2015-07-30'
 			},
 			{
-				no			: 2,
+				idx			: 2,
 				email 	 	: 'soultomind930@gmail.com',
 				name 		: '이바우',
 				job 		: 'web developer',
@@ -43,7 +64,7 @@ var member = {
 				updateDate 	: '2015-07-30'
 			},
 			{
-				no			: 3,
+				idx			: 3,
 				email 	 	: 'jusaha1109@gmail.com',
 				name 		: '김태영',
 				job 		: 'web developer',
@@ -51,7 +72,7 @@ var member = {
 				updateDate 	: '2015-07-30'
 			},
 			{
-				no			: 4,
+				idx			: 4,
 				email  		: 'usdrd90@gmail.com',
 				name 		: '전옥현',
 				job 		: 'web publisher',
@@ -59,7 +80,7 @@ var member = {
 				updateDate 	: '2015-07-30'
 			},
 			{
-				no			: 5,
+				idx			: 5,
 				email 	 	: 'talkyfull@gmail.com',
 				name 		: '신윤아',
 				job 		: 'web publisher',
@@ -78,7 +99,7 @@ var member = {
 		$.each(members, function(index, member){
 			var $tr = $(document.createElement('tr'));
 			$tr.addClass('member_info');
-			$tr.attr('id', 'member_'+member.no);
+			$tr.attr('id', 'member_' + member.idx);
 
 			for(prop in member){
 				var $td = $(document.createElement('td'));
@@ -90,21 +111,59 @@ var member = {
 		$table.append($tbody);
 	},
 
-	find : function(target){
-		var $target = $(target),
-			members = this.list,
-			no = $target.attr('id').slice(7);
+	find : function(idx){
+		var members = this.list,
+			res = null;
 
 		$.each(members, function(index, member){
-			if(member.no == no){
-				return member;
+			if(member.idx == idx){
+				res = member;
+				return false;
 			}
 		});
+
+		return res;
 	},
 	
 	edit : function(member){
-		var member = member || {};
+		var member = member,
+			$inputEmail = $('#inputEmail'),
+			$inputName = $('#inputName'),
+			$inputJob = $('#inputJob');
 
+		this.currentData = member;
+		$inputEmail.val(member.email);
+		$inputName.val(member.name);
+		$inputJob.val(member.job);
+	},
+
+	save : function(){
+		var member = this.currentData,
+			$inputEmail = $('#inputEmail'),
+			$inputName = $('#inputName'),
+			$inputJob = $('#inputJob');
+
+		if(!member.no){
+			member.no = this.generateIdx();
+		}
+
+		member.email = $inputEmail.val();
+		member.name = $inputName.val();
+		member.job = $inputJob.val();
+		member.updateDate = this.dateFormat();
+
+		this.list.push(member);
+		//this.init();
+	},
+
+	generateIdx : function(){
+		var lastIdx = this.list[this.list.length-1].idx;
+		return lastIdx+1;
+	},
+
+	dateFormat : function(date){
+		var date = date || new Date();
+		return date.getFullYear() + '-' + (date.getMonth()+1) + '-' + (date.getDate());
 	}	
 
 }
