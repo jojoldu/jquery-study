a1a17137a85efc0e4dde35b53f10c3d1fdd7adb9
이동욱 <jojoldu@zuminternet.com>
2015-09-01 17:34:53 +0900

    로그인 기능 작업중

--- js/user.js
+++ js/user.js
@@ -10,9 +10,14 @@ $(function(){
 var user = {
 	$el : {},
 
+	//기본적인 버튼 이벤트 binding
 	init : function(){
-		//기본적인 버튼 이벤트 binding
+		
 		this.$el = $('.container');
+
+		this.$el.find('#btnLogin').click(function(){
+			user.login();
+		});
 
 		this.$el.find('#btnSignUp').click(function(){
 			user.showModal();
@@ -37,10 +42,7 @@ var user = {
 	},
 
 	resetModal : function(){
-		this.$el.find('#inputEmail').val('');
-		this.$el.find('#inputPassword').val('');
-		this.$el.find('#inputName').val('');
-		this.$el.find('#inputJob').val('');
+		this.$el.find('.signForms').val('');
 	},
 
 	signUp : function(){
@@ -48,30 +50,48 @@ var user = {
 			password = this.$el.find('#inputPassword').val(),
 			name = this.$el.find('#inputName').val(),
 			job = this.$el.find('#inputJob').val();
-
-		if(this.find(email)){
-			alert('이미 가입된 사용자입니다.');
-			return;
+		
+		var obj = {
+			email : email
 		}
 
 		if(!this.validate()){
 			alert('필수값을 모두 채워주세요');
 			return;
 		}
+
+		if(this.find(obj)){
+			alert('이미 가입된 사용자입니다.');
+			return;
+		}
+	
+		obj.password = password;
+		obj.name = name;
+		obj.job = job;
 		
-		this.save({ email : email, password : password, name : name, job : job});
+		this.save(obj);
 	},
 	
 	//DB 연동시 수정
-	find : function(email){
+	find : function(obj){
 		var result;
 
 		$.each(users, function(index, value){
-			if(value.email === email){
-				result = value;
-				return false;
+			
+			if(obj.password){ //로그인시
+				if(value.email === obj.email && value.password === obj.password){
+					result = value;
+					return;
+				}		
+			}else{ //가입 중복체크시
+				if(value.email === email){
+					result = value;
+					return;
+				}
 			}
+
 		});
+		
 
 		return result;
 	},
@@ -80,10 +100,14 @@ var user = {
 		var $signForms = this.$el.find('.signForms'),
 			result = true;
 
-		$.each($signForms, function(index, value){
-			if(!value.val()){
-				value.addClass('empty');
+		$.each($signForms, function(index, signForm){
+			var $signForm = $(signForm);
+
+			if(!$signForm.val()){
+				$signForm.addClass('empty');
 				result = false;
+			}else{
+				$signForm.removeClass('empty');
 			}
 		});
 
@@ -109,6 +133,13 @@ var user = {
 	},
 
 	login : function(){
+		var loginUser = {},
+			loginEmail = this.$el.find('#loginEmail').val(),
+			loginPassword = this.$el.find('#loginPassword').val();
+
+		loginUser.email = loginEmail;
+		loginUser.password = loginPassword;
+
 
 	}
 }
