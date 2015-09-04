56890020c0adcaa2a2b759887ba911f9252a0e90
jojoldu <jojoldu@gmail.com>
2015-09-05 00:33:33 +0900

    회원가입 기초 함수 생성 및 질문 작성

---
+++ js/user.js
@@ -0,0 +1,65 @@
+
+
+$(function(){
+
+	//js 호출시 바로 시작될 영역
+	user.init();
+});
+
+
+var user = {
+
+	$el : {},
+
+	init : function(){
+		
+		//기본적인 이벤트 바인딩
+
+		this.$el = $('.container');
+
+		this.$el.find('#btnSignUp').click(function(){
+			user.showModal();
+		});
+
+		this.$el.find('#btnClose').click(function(){
+			user.closeModal();
+		});	
+
+		this.$el.find('#btnSubmit').click(function(){
+			user.signUp();
+		});
+
+
+	},
+
+	showModal : function(){
+		this.resetModal();
+		this.$el.find('#userModal').modal();
+	},
+	
+	closeModal : function(){
+		this.$el.find('#userModal').modal('hide');
+	},
+
+	resetModal : function(){
+		this.$el.find('.signForms').val('');
+	},
+
+	signUp : function(){
+		var email = this.$el.find('#inputEmail').val(),
+			password = this.$el.find('#inputPassword').val(),
+			passwordConfirm = this.$el.find('#inputPasswordConfirm').val(),
+			name = this.$el.find('#inputName').val(),
+			job = this.$el.find('#inputJob').val();
+		
+
+		// 1. 입력창에서 빈칸은 없는가?
+
+		// 2. password와 passwordConfirm이 같은가?
+
+		// 3. 이미 등록된 사용자가 아닌가?
+
+		// 4. 위 검증이 끝나면 회원 가입
+	}
+
+}
