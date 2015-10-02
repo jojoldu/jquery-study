9e72d564df7f2569eebf950ad2b84e7dbd1d4d17
jojoldu <jojoldu@gmail.com>
2015-10-03 06:29:58 +0900

    app.js 생성 및 json 테스트

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+
+/*
+	DB 연동시 삭제될 부분
+*/
+var currentTime = new Date();
+var users = [{
+		email : '1',
+		password : '1',
+		name : '1',
+		job : '1',
+		joinDate : currentTime,
+		updateDate : currentTime
+},
+{
+		email : '2',
+		password : '2',
+		name : '2',
+		job : '2',
+		joinDate : currentTime,
+		updateDate : currentTime
+}];
+
+
+app.get('/user', function(req, res) {
+    res.send(users);
+});
+
+  
+app.listen(8080);
+console.log('Express Listening on port 8080...');
