9949ae7651e33772870c9d9e7383fe90c820d536
이동욱 <jojoldu@zuminternet.com>
2015-10-02 14:10:45 +0900

    HTML 렌더링 작업중

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express'),
 	bodyParser = require('body-parser');
 var app = express();
+var path = require('path');
 
+/*
+	DB 연동시 삭제될 부분
+*/
+var currentTime = new Date();
 var users = [{
 		email : '1',
 		password : '1',
@@ -19,19 +24,30 @@ var users = [{
 		updateDate : currentTime
 }];
 
+
+
+app.get('/login', function(req, res){
+	res.sendFile(path.join(__dirname + '/view/login.html'));
+})
+
 app.get('/user', function(req, res) {
+	res.header("Content-Type", "application/json; charset=utf-8");
     res.send(users);
 });
 
 app.get('/user/:id', function(req, res) {
 	var result = {
 					id : req.params.id,
-					name : req.params.id + '님의 이름',
+					name : req.params.id,
 					age : '29'
 				 };
-    res.jsonp(result);
+	res.header("Content-Type", "application/json; charset=utf-8");
+    res.json(result);
 });
 
+app.post('/user', function(req, res){
+	
+});
   
-app.listen(8087);
-console.log('Express Listening on port 8087...');
+app.listen(8080);
+console.log('Express Listening on port 8080...');
